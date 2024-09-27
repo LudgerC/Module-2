c86c4d09a071712febec9653c2d2c6fa5679b262
LudgerC <ludgerfilypecardoso@gmail.com>
2024-09-28 00:38:02 +0200

    Update @2.mjs

--- @2.mjs
+++ @2.mjs
@@ -2,3 +2,70 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
+
+let jaartal = parseFloat(await userInput.question("Geef een jaartal: "));
+let maand = await userInput.question("Geef een maand: ");
+
+
+switch(maand)
+{
+    case "januari":
+        console.log("In het jaar " + jaartal + " heeft " + maand + " heeft 30 dagen");
+    break;
+
+    case "februari":
+        if((jaartal % 2 == 0 && jaartal % 100 !== 0) || (jaartal % 400 == 0))
+        {
+            console.log("In het jaar " + jaartal + " heeft " + maand + " 29 dagen");
+        }
+        else
+        {
+            console.log("In het jaar " + jaartal + " heeft " + maand + " 28 dagen");
+        }
+    break;
+
+    case "maart":    
+        console.log("In het jaar " + jaartal + " heeft " + maand + " 31 dagen");
+    break;
+
+    case "april":
+        console.log("In het jaar " + jaartal + " heeft " + maand + " 30 dagen");
+    break;
+
+    case "mei":
+        console.log("In het jaar " + jaartal + " heeft " + maand + " 31 dagen");
+    break;
+
+    case "juni":
+        console.log("In het jaar " + jaartal + " heeft " + maand + " 30 dagen");
+    break;
+
+    case "juli":
+        console.log("In het jaar " + jaartal + " heeft " + maand + " 31 dagen");
+    break;
+    
+    case "augustus":
+        console.log("In het jaar " + jaartal + " heeft " + maand + " 31 dagen");
+    break;
+    
+    case "september":    
+        console.log("In het jaar " + jaartal + " heeft " + maand + " 30 dagen");
+    break;
+
+    case "oktober":
+        console.log("In het jaar " + jaartal + " heeft " + maand + " 31 dagen");
+    break;
+    
+    case "november":
+        console.log("In het jaar " + jaartal + " heeft " + maand + " 30 dagen");
+    break;
+    
+    case "december":
+        console.log("In het jaar " + jaartal + " heeft " + maand + " 31 dagen");
+    break;
+
+    default:
+        console.log("Ongeldige keuze! ");
+    break;
+
+}

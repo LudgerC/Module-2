6d18fd84a1841da8873121304492c69494316e73
LudgerC <ludgerfilypecardoso@gmail.com>
2024-09-27 11:17:45 +0200

    Create E1.mjs

---
+++ E1.mjs
@@ -0,0 +1,14 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let getal = parseFloat(await userInput.question("Geef een getak: "));
+
+if(getal % 2 == 0)
+{
+    console.log("Het getal is even! ")
+}
+else 
+{
+    console.log("Het getal is oneven! ")
+}
